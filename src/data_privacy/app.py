a47b0414df45bbf48490f7be664b5a3e1a785c21
obahamondeglobant <o.bahamonde@globant.com>
2024-05-01 19:08:03 -0500

    test

--- src/data_privacy/app.py
+++ src/data_privacy/app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+
 def main_loop():
-	st.title("Data Privacy App")
- 
+    st.title("Data Privacy App")
+    text = st.text_area("Enter your text here:")
+    st.write("You entered:", text)
+
 
 if __name__ == "__main__":
 	main_loop()
